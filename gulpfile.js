534be64b21005d003fb990ece42b5ffad265e8d2
katatonia <pmilekhina@gmail.com>
2024-10-14 15:04:40 +0300

    fix: вносит правки в настройки gulp и другие файлы

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-const gulp         = require('gulp');
-const browserSync  = require('browser-sync');
-const sass         = require('gulp-sass')(require('sass'));
-const rename       = require('gulp-rename');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCss     = require('gulp-clean-css');
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import sass from 'gulp-sass';
+import sassCompiler from 'sass'; // Импортируем компилятор Sass
+import rename from 'gulp-rename';
+import autoprefixer from 'gulp-autoprefixer';
+import cleanCss from 'gulp-clean-css';
+
+// Инициализируем gulp-sass с компилятором
+const compileSass = sass(sassCompiler);
 
 // Server
 gulp.task('server', function() {
@@ -17,16 +21,16 @@ gulp.task('server', function() {
 // Styles
 gulp.task('styles', function() {
     return gulp.src('src/scss/**/*.+(scss|sass)')
-            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-            .pipe(rename({
-                prefix: '',
-                suffix: '.min',
-              }))
-            .pipe(autoprefixer())
-            .pipe(cleanCss({compatibility: 'ie8'}))
-            .pipe(gulp.dest('src/css'))
-            .pipe(browserSync.stream());
-})
+        .pipe(compileSass({ outputStyle: 'compressed' }).on('error', compileSass.logError))
+        .pipe(rename({
+            prefix: '',
+            suffix: '.min',
+        }))
+        .pipe(autoprefixer())
+        .pipe(cleanCss({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+});
 
 // Watcher
 gulp.task('watch', function() {
@@ -35,5 +39,5 @@ gulp.task('watch', function() {
     gulp.watch('src/js/*.js').on('change', browserSync.reload);
 });
 
-
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+
