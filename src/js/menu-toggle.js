bf925b4200da7639743a33b3822ab4ffb3220254
katatonia <pmilekhina@gmail.com>
2024-10-31 16:53:25 +0300

    feature: допиливает дизайн

--- src/js/menu-toggle.js
+++ src/js/menu-toggle.js
@@ -1,5 +1,6 @@
 const burgerButton = document.querySelector('.header__burger');
 const nav = document.querySelector('.header__nav');
+const projectsList = document.querySelector('.projects__list');
 const body = document.body;
 
 const toggleMenu = () => {
@@ -7,9 +8,8 @@ const toggleMenu = () => {
         nav.classList.toggle('header__nav_active');
         burgerButton.classList.toggle('header__burger_active');
         body.classList.toggle('lock');
+        projectsList.classList.toggle('projects__list_hidden')
     });
 }
 
 toggleMenu();
-
-
