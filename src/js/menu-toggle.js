4d36c8cac1816536cc92279305623594b94e4124
katatonia <pmilekhina@gmail.com>
2024-10-31 17:40:54 +0300

    fix

--- src/js/menu-toggle.js
+++ src/js/menu-toggle.js
@@ -1,6 +1,5 @@
 const burgerButton = document.querySelector('.header__burger');
 const nav = document.querySelector('.header__nav');
-const projectsList = document.querySelector('.projects__list');
 const body = document.body;
 
 const toggleMenu = () => {
@@ -8,7 +7,6 @@ const toggleMenu = () => {
         nav.classList.toggle('header__nav_active');
         burgerButton.classList.toggle('header__burger_active');
         body.classList.toggle('lock');
-        projectsList.classList.toggle('projects__list_hidden')
     });
 }
 
